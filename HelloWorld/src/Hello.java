32796a0bb3378cc7e8dae72e4e4a7d6202b47b0e
DavidLapadula <lapadula.david@gmail.com>
2019-10-23 22:40:20 -0400

    [getting started] working with expressions and simple variable names

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main (String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello, David");
+        
+        int myFirstNumber =  (10 + 5) + (2 * 10);
+        System.out.println("Your first number is " + myFirstNumber);
     }
 }
