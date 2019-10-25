099990b919607d676f79bebc4d0b4f5fe4fd7402
DavidLapadula <lapadula.david@gmail.com>
2019-10-24 22:25:20 -0400

    worked with all primitive data types. Byte, short, long

--- ByteSHortIntLong/src/com/company/Main.java
+++ ByteSHortIntLong/src/com/company/Main.java
@@ -3,6 +3,36 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	 int my value
+	 int myValue = 10000;
+
+	    int myMinIntValue = Integer.MIN_VALUE;
+	    int myMaxIntValue = Integer.MAX_VALUE + 1;
+
+		System.out.println("Integer min "  + myMinIntValue);
+		System.out.println("Integer max "  + myMaxIntValue);
+		System.out.println("Max overload" + (myMaxIntValue + 1));
+		System.out.println("Min overload" + (myMinIntValue - 1 ));
+
+		byte myMinByteValue = Byte.MIN_VALUE;
+		byte myMaxByteValue = Byte.MAX_VALUE;
+
+		System.out.println("Byte min value " + myMinByteValue);
+		System.out.println("Byte max value " + myMaxByteValue);
+
+		short myMinShortValue = Short.MIN_VALUE;
+		short myMaxShortValue = Short.MAX_VALUE;
+
+		System.out.println("Short min value " + myMinShortValue);
+		System.out.println("Short max value " + myMaxShortValue);
+
+		long myLongValue = 100L;
+
+		long myMinLongValue = Long.MIN_VALUE;
+		long myMaxLongValue = Long.MAX_VALUE;
+
+		System.out.println("Long min value " + myMinLongValue);
+		System.out.println("Long max value " + myMaxLongValue);
+
+
     }
 }
