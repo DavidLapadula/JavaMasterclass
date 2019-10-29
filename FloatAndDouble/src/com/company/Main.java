5bf907683a305d4d6bab0fa6cba92398f9529c74
DavidLapadula <lapadula.david@gmail.com>
2019-10-28 22:16:11 -0400

    [getting started] simple arithmetic with float and double primitve types

--- FloatAndDouble/src/com/company/Main.java
+++ FloatAndDouble/src/com/company/Main.java
@@ -14,5 +14,18 @@ public class Main {
 
         System.out.println("Double min " + minDoubleValue);
         System.out.println("Double max " + maxDoubleValue);
+
+        int myIntValue = 5 / 3;
+        float myFloatValue = 5f / 3f;
+        double myDoubleValue = 5d / 3d;
+
+        System.out.println("My int Value " + myIntValue);
+        System.out.println("My float Value " + myFloatValue);
+        System.out.println("My double Value " + myDoubleValue);
+
+        // Pounds to kilos
+        double pounds = 200d;
+        double kilos = pounds * 0.45359237d;
+        System.out.println("Pounds to Kilos " + kilos);
     }
 }
