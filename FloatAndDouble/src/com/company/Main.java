3d28a39635651349409e7fa595ae69fbc0e75d12
DavidLapadula <lapadula.david@gmail.com>
2019-10-28 21:58:44 -0400

    [getting started] new project for floating point numbers

---
+++ FloatAndDouble/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    float minFloatValue = Float.MIN_VALUE;
+	    float maxFloatValue = Float.MAX_VALUE;
+
+        System.out.println("FLoat min " + minFloatValue);
+        System.out.println("FLoat max " + maxFloatValue);
+
+        double minDoubleValue = Double.MIN_VALUE;
+	    double maxDoubleValue = Double.MAX_VALUE;
+
+        System.out.println("Double min " + minDoubleValue);
+        System.out.println("Double max " + maxDoubleValue);
+    }
+}
