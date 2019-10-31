36e419994db9a04c04b854c911594ac5cba626c7
DavidLapadula <lapadula.david@gmail.com>
2019-10-30 22:47:55 -0400

    [getting started] did additional testing with string  class

--- CharAndBoolean/src/com/company/Main.java
+++ CharAndBoolean/src/com/company/Main.java
@@ -3,6 +3,33 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Char primitve type
+            char myChar = 'D';
+            char myUnicode = '\u0044';
+
+            System.out.println(myChar);
+            System.out.println(myUnicode);
+
+        // Boolean primitive type
+            boolean myTrueValue = true;
+            boolean myFalseValue = false;
+
+        //  String class
+            String myString = "This is a string";
+            System.out.println("My statement is " + myString);
+
+            myString = myString + " and some other stuff";
+            System.out.println("My statement is " + myString);
+
+            String numberString = "205.55";
+            numberString = numberString + "49.95"; // will not add anything, will only append it to the end
+
+            String lastString = "10";
+            int myInt = 50;
+            lastString = lastString + myInt;
+            System.out.println("Last String equal to " + lastString);
+
+
     }
 }
