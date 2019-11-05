6806d32092f8f0e4400b786783a2a2bdad98c8c4
DavidLapadula <lapadula.david@gmail.com>
2019-11-04 22:30:47 -0500

    [statement expressions whitespace indenting] added simple control methods to test

--- IfKeywordAndCodeBlocks/src/com/company/Main.java
+++ IfKeywordAndCodeBlocks/src/com/company/Main.java
@@ -3,6 +3,27 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    boolean gameOver = true;
+	    int score = 5000;
+	    int levelCompleted = 5;
+	    int bonus = 100;
+
+	    if (score == 5000) {
+            System.out.println("Your score was 5000");
+        } else {
+            System.out.println("Your score was not 5000");
+        };
+
+	    // if will catch the line after it even without the braces
+	    if (score == 500)
+            System.out.println("Your score was 5000");
+
+	    if (gameOver) {
+	        int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Final score is " + finalScore);
+        };
+
+
+
     }
 }
