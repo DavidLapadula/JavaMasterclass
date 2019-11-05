5802d680a49154325bc60e976eec1d60ac0d41f5
DavidLapadula <lapadula.david@gmail.com>
2019-11-04 21:57:05 -0500

    [Expressions statements blocks methods] initialized new project for expresions

---
+++ KeywordsExpressions/src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // mile is equal to 1.609344 kilometres
+        double kilometers = (100 * 1.609344);
+    }
+}
