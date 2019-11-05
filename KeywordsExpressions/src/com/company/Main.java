113ed7877f192664e915d68bd870b0b6dacdd4ae
DavidLapadula <lapadula.david@gmail.com>
2019-11-04 22:01:06 -0500

    [statement expressions whitespace indenting] initizlied new project for white space and indenting

--- KeywordsExpressions/src/com/company/Main.java
+++ KeywordsExpressions/src/com/company/Main.java
@@ -5,5 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	    // mile is equal to 1.609344 kilometres
         double kilometers = (100 * 1.609344);
+
+        System.out.println(kilometers + " is how many km are in a mile");
+
     }
 }
