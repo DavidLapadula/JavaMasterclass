eb6ac99687bd24c10510819441ae21c5623f5526
DavidLapadula <lapadula.david@gmail.com>
2019-11-05 22:06:59 -0500

    [statement expressions whitespace indenting] simple method made for testing

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -3,6 +3,20 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Main executed");
+        boolean gameOver = true;
+        int score = 800;
+        int levelCompleted = 5;
+        int bonus = 800;
+        calculateScore(gameOver, score, levelCompleted, bonus);
+        calculateScore(true, 10000, 8, 200);
     }
+
+    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+        if (gameOver) {
+            int finalScore = score + (levelCompleted * bonus);
+            finalScore += 1000;
+            System.out.println("Your final score is " + finalScore);
+        }
+    };
 }
