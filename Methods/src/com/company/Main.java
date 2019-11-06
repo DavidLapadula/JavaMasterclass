00a906124dcace59f36921759740c8197e22b46c
DavidLapadula <lapadula.david@gmail.com>
2019-11-05 22:28:01 -0500

    [statement expressions whitespace indenting] method challenge complete

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -4,19 +4,41 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Main executed");
+
+        // Simple Method testing
         boolean gameOver = true;
         int score = 800;
         int levelCompleted = 5;
         int bonus = 800;
-        calculateScore(gameOver, score, levelCompleted, bonus);
-        calculateScore(true, 10000, 8, 200);
+        int scoreOne = calculateScore(gameOver, score, levelCompleted, bonus);
+        int scoreTwo = calculateScore(true, 10000, 8, 200);
+
+        // Challenge
+        displayHighScorePosition("David", 1);
+        System.out.println("Highest is " + calculateHighScorePosition(1500));
+        System.out.println("Second Highest is " + calculateHighScorePosition(900));
+        System.out.println("Third Highest is " + calculateHighScorePosition(400));
+        System.out.println("Lowest is " + calculateHighScorePosition(50));
+
     }
 
-    public static void calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
+    public static int calculateScore(boolean gameOver, int score, int levelCompleted, int bonus) {
         if (gameOver) {
             int finalScore = score + (levelCompleted * bonus);
             finalScore += 1000;
-            System.out.println("Your final score is " + finalScore);
+            return finalScore;
         }
+        return -1;
+    };
+
+    public static void displayHighScorePosition(String player, int position) {
+        System.out.println(player + " got into positon " + position + " on the high score table");
+    }
+
+    public static int calculateHighScorePosition(int score) {
+         if (score >= 1000 ) return 1;
+         if (score >= 500 && score < 1000 ) return 2;
+         if (score >= 100 && score < 500 ) return 3;
+         return 4;
     };
 }
