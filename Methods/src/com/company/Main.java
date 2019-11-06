19a35db38c4a0c7377d9a6fae0e8ebd39c9fc8dc
DavidLapadula <lapadula.david@gmail.com>
2019-11-05 22:33:14 -0500

    [statement expressions whitespace indenting] minor refactor on method challenge

--- Methods/src/com/company/Main.java
+++ Methods/src/com/company/Main.java
@@ -36,9 +36,10 @@ public class Main {
     }
 
     public static int calculateHighScorePosition(int score) {
-         if (score >= 1000 ) return 1;
-         if (score >= 500 && score < 1000 ) return 2;
-         if (score >= 100 && score < 500 ) return 3;
-         return 4;
+        int position = 4;
+         if (score >= 1000 ) position =  1;
+         if (score >= 500 ) position =  2;
+         if (score >= 100 ) position =  3;
+         return position;
     };
 }
