2eaa9e4a2ac64eb8db1284b94f977483a591a0ec
DavidLapadula <lapadula.david@gmail.com>
2019-11-06 22:29:57 -0500

    [statement expressions whitespace indenting] added solution to speed converter problem

--- MethodChallenges/src/com/company/Main.java
+++ MethodChallenges/src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles " + miles);
+
+        SpeedConverter.printConversion(10.5);
     }
 }
