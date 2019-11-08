aa8f13aead9d8d3bcffff16f61e027cdb22e846c
DavidLapadula <lapadula.david@gmail.com>
2019-11-07 22:20:47 -0500

    [statement expressions whitespace indenting] KB to MB challenge complete

--- MethodChallenges/src/com/company/Main.java
+++ MethodChallenges/src/com/company/Main.java
@@ -3,9 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        // Speed Converter Challenge
+
+        System.out.println("Speed Converter Test");
         long miles = SpeedConverter.toMilesPerHour(10.5);
         System.out.println("Miles " + miles);
-
         SpeedConverter.printConversion(10.5);
+        System.out.println("--------------------------------");
+
+        // KB to MD Challenge
+        System.out.println("KB to MB Challenge");
+        MegabytesConverter.printMegaBytesAndKiloBytes(2500);
+        MegabytesConverter.printMegaBytesAndKiloBytes(0);
+        MegabytesConverter.printMegaBytesAndKiloBytes(-5);
+        System.out.println("--------------------------------");
+
+        // Barking Dog Challenge
+        System.out.println("Barking Dog Challenge");
+        System.out.println("--------------------------------");
     }
 }
