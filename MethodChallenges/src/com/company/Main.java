c7545c64f3754894b14fb8973f9a552ef6944a94
DavidLapadula <lapadula.david@gmail.com>
2019-11-10 18:38:14 -0500

    Decimal comparison operator challege complete

--- MethodChallenges/src/com/company/Main.java
+++ MethodChallenges/src/com/company/Main.java
@@ -33,5 +33,14 @@ public class Main {
         System.out.println(LeapYear.isLeapYear(2017));
         System.out.println(LeapYear.isLeapYear(2000));
         System.out.println("--------------------------------");
+
+        //Decimal Operator
+        System.out.println("Decimal Operator Challenge");
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.1756, -3.175));
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.175,3.176));
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(3.0,3.0));
+        System.out.println(DecimalComparator.areEqualByThreeDecimalPlaces(-3.123,3.123));
+        System.out.println("--------------------------------");
+
     }
 }
