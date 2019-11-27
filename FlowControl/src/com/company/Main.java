9315180da1f5789e26d5f8b2a5b68793acf5dc3d
DavidLapadula <lapadula.david@gmail.com>
2019-11-26 22:32:52 -0500

    [Flow Control] added methods for checking leap years and number of days in a month

--- FlowControl/src/com/company/Main.java
+++ FlowControl/src/com/company/Main.java
@@ -4,7 +4,36 @@ public class Main {
 
     public static void main(String[] args) {
         // Switch methods
+        System.out.println("Basic Challenges");
 	    Switch.basicSwitch();
 	    Switch.charSwitch();
+        System.out.println("---------------------------");
+
+        // Number in Word challenge
+        System.out.println("Number in Word challenge");
+	    Switch.printNumberInWord(1);
+        Switch.printNumberInWord(2);
+        Switch.printNumberInWord(3);
+        Switch.printNumberInWord(-1);
+        System.out.println("---------------------------");
+
+        // Leap Year challenge
+        System.out.println("Leap Year Challenge");
+        System.out.println(Switch.isLeapYear(-1600));
+        System.out.println(Switch.isLeapYear(1600));
+        System.out.println(Switch.isLeapYear(2017));
+        System.out.println(Switch.isLeapYear(2000));
+        System.out.println("---------------------------");
+
+        // Days in Month Challenge
+        System.out.println("Days in month challenge");
+        System.out.println(Switch.getDaysInMonth(1, 2020));
+        System.out.println(Switch.getDaysInMonth(2, 2020));
+        System.out.println(Switch.getDaysInMonth(2, 2018));
+        System.out.println(Switch.getDaysInMonth(-1, 2020));
+        System.out.println(Switch.getDaysInMonth(1, -2020));
+        System.out.println("---------------------------");
+
+
     }
 }
--- FlowControl/src/com/company/Switch.java
+++ FlowControl/src/com/company/Switch.java
@@ -32,4 +32,102 @@ public class Switch {
                 break;
         }
     }
+
+    public static void printNumberInWord(int number) {
+        switch (number) {
+            case 0:
+                System.out.println("ZERO");
+                break;
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            case 7:
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
+                break;
+            default:
+                System.out.println("OTHER");
+                break;
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        boolean isLeapYear = false;
+
+        if (year >= 1 && year <= 9999) {
+            isLeapYear =  (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        };
+
+        return isLeapYear;
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        int daysInMonth = -1;
+
+        if((month >= 1 && month <= 12) && (year >= 1 && year <= 9999)) {
+            switch (month) {
+                case 1:
+                    daysInMonth = 31;
+                    break;
+                case 2:
+                    if (isLeapYear(year)) {
+                        daysInMonth = 29;
+                    } else {
+                        daysInMonth = 28;
+                    }
+                    break;
+                case 3:
+                    daysInMonth = 31;
+                    break;
+                case 4:
+                    daysInMonth = 30;
+                    break;
+                case 5:
+                    daysInMonth = 31;
+                    break;
+                case 6:
+                    daysInMonth = 30;
+                    break;
+                case 7:
+                    daysInMonth = 31;
+                    break;
+                case 8:
+                    daysInMonth = 31;
+                    break;
+                case 9:
+                    daysInMonth = 30;
+                    break;
+                case 10:
+                    daysInMonth = 31;
+                    break;
+                case 11:
+                    daysInMonth = 30;
+                    break;
+                case 12:
+                    daysInMonth = 31;
+            }
+        }
+
+        return daysInMonth;
+    }
 }
