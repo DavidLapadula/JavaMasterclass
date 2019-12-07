8c3c5247fbf8793205a5224754e29b11d8daf8a1
DavidLapadula <lapadula.david@gmail.com>
2019-12-07 10:09:34 -0500

    Added diagonal start challenge

--- FlowControl/src/com/company/DiagonalStar.java
+++ FlowControl/src/com/company/DiagonalStar.java
@@ -4,9 +4,21 @@ public class DiagonalStar {
 
     public static void printSquareStar(int number) {
         if (number >= 5) {
-            System.out.println(">>>>>>>>>>>>>>>>>>>");
-            for (int col = 0; col < number; col++) {
-                System.out.println("*");;
+            for (int col = 0; col < number; col ++) {
+                 for (int row = 0; row < number; row ++) {
+                     if (
+                         (row == 0 || row == number - 1) ||
+                         (col == 0 || col == number - 1) ||
+                         (row == col) ||
+                         (col == (number - (row + 1)))
+                     )
+                     {
+                         System.out.print("*");
+                     } else {
+                         System.out.print(" ");
+                     };
+                 };
+                System.out.println();
             };
         } else {
             System.out.println("Invalid Value");
