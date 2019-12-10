16721e01c17079fe86d2a2a235ec0b222a64a4ad
DavidLapadula <lapadula.david@gmail.com>
2019-12-09 22:25:46 -0500

    [OOP1] Simple car class created with getters and setters

--- Classes/src/com/company/Main.java
+++ Classes/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Car porsche = new Car();
+	    porsche.setModel("Carrera");
     }
 }
