44a5663cc4420feefff79a0bca92b30003b4f611
DavidLapadula <lapadula.david@gmail.com>
2019-12-10 22:06:33 -0500

    [OOP-1] Basic class work using constructors and overloading

--- Classes/src/com/company/Main.java
+++ Classes/src/com/company/Main.java
@@ -3,7 +3,23 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+
+        // Using test class
 	    Car porsche = new Car();
 	    porsche.setModel("Carrera");
+
+	    // Test account features
+        Account davidAccount = new Account();
+        davidAccount.withDrawal(100);
+
+        Account otherAccount = new Account("123456", 0.00, "David", "david@email.com", "4165569985");
+
+        System.out.println(otherAccount.getCustomerName());
+
+        Account nextAccount = new Account();
+
+        System.out.println(nextAccount.getCustomerName());
+
+
     }
 }
