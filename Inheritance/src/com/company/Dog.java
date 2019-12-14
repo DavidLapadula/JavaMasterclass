abebfac1161dc398c90d856cc8e93e6bf2eb4c13
DavidLapadula <lapadula.david@gmail.com>
2019-12-14 11:22:12 -0500

    [OOP-1] Basic inheritance complete with generic animal template

---
+++ Inheritance/src/com/company/Dog.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.sql.SQLOutput;
+
+public class Dog extends Animal {
+
+    private int eyes;
+    private int legs;
+    private int tail;
+    private int teeth;
+    private String coat;
+
+    public Dog( int size, int weight, String name, int eyes, int legs, int tail, int teeth, String coat) {
+        super(1, 1, size, weight, name);
+        this.eyes = eyes;
+        this.legs = legs;
+        this.tail = tail;
+        this.teeth = teeth;
+        this.coat = coat;
+    }
+
+    private void chew() {
+        System.out.println("Dog chew called");
+    };
+
+    @Override
+    public void eat() {
+        System.out.println("Dog.eat called");
+        chew();
+        super.eat();
+    }
+}
