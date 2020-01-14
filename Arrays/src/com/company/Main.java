76bd30db19cf646b4226af9bd0003052605fa30d
DavidLapadula <lapadula.david@gmail.com>
2020-01-13 22:38:27 -0500

    [ArraysLists] Simple array challenge with sorting function;

--- Arrays/src/com/company/Main.java
+++ Arrays/src/com/company/Main.java
@@ -2,6 +2,17 @@ package com.company;
 
 import java.util.Scanner;
 
+/*
+ Array notes
+    - DS of piece of connected memory that can be accessed by index
+    - Need to pass in amount and type
+    - [] are array accessors
+    - Use new operator to initialize arrays - int set to 0, String set to null, boolean set to false
+    - Use {} to make anonymous array, implicitly set length and values
+    - If use the wrong index then will give you out of bounds error
+    - <= to avoid out of bounds error to last value in array because is zero indexed
+ */
+
 public class Main {
 
     private static Scanner scanner = new Scanner(System.in);
@@ -12,7 +23,7 @@ public class Main {
         myIntArray[5] = 50;
 
         int[] myIntegers = getIntegers(5);
-        for (int i=0; i < myIntegers.length; i++) {
+        for (int i = 0; i < myIntegers.length; i++) {
             System.out.println(myIntegers[i]);
         }
     };
@@ -22,6 +33,7 @@ public class Main {
         int[] values = new int [number];
 
         for (int i = 0; i < values.length; i++) {
+            // Next int allows you to get the next value from being input into the console
             values[i] = scanner.nextInt();
         }
          return values;
