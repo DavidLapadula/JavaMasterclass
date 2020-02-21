84bbb14cb1e74aac832c18793e2841a5d9ee821f
DavidLapadula <lapadula.david@gmail.com>
2020-02-20 21:47:38 -0500

    [ConvPackKeyw] Initialized project and practiced naming conventions

---
+++ ConvPackKeyw/src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    /*
+	        Can name packages
+	           - lower case, domain name reversed
+	           - Cannot have dash in package name
+	           - Keywords in java in domain should start with _. i.e switch.com --> com._switch
+           Class Names
+                - Camel Case, nouns, start with capitol letter
+            Interface
+                - Capital, consider what the methods that implement them will be able to do
+            Methods
+                - Mixed Case, verb, reflect the function performed OR result returned
+            Constants
+                - Screaming snake case, and declared using the final keyword
+            Variable Names
+                - mixedCase, with meaningful name
+            Type Parameters
+                - single character capital letter --> like 'T' for generic
+                E (Element) K (key) T (Type) V (Value)
+	     */
+    }
+}
