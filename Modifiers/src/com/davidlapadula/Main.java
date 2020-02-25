bd271936a705a1bde51a70b30c7c1d9c12a65503
DavidLapadula <lapadula.david@gmail.com>
2020-02-24 22:09:50 -0500

    [ConvPackKeyw] Praticed final keyword and static initializers

--- Modifiers/src/com/davidlapadula/Main.java
+++ Modifiers/src/com/davidlapadula/Main.java
@@ -21,6 +21,23 @@ public class Main {
             - This is why psvm is static; before program runs, no instances, so needs to call method which does not require instance to work
                 - Can run from command line with java com.davidlapadula.Main - Main does not have to be entry point with psvm but is convention for ease
             - Non static method or field cannot be referenced from non-static context BECAUSE static fields do not need class instance to be referenced - so non-static fields referenced there might not exist yet
+
+        - Final
+            - Used when need value that should not change after initialization
+            - Can only be modified once and it must be before class constructor finishes
+            - Should be static, because if it is not supposed to change then it should be at class level and not make a new one with every instance
+            - Can be assigned when it is declared or in constructor (maybe value is result of calculation)
+            - Not available to be changed by other classes
+            - Like every instance of class having a unique ID
+            - If mark class as final, cannot make a subclass either; so other classes cannot extend it
+            - Can override methods UNLESS use final keyword so child classes cannot override the method
+
+        - Static Initializations
+            - Constructor whenever class gets instantiated
+            - Static initializer: only loaded once when class is first loaded into the project
+            - Static final variable can be assigned in initialization block, but must be completed once this block has run
+            - Can make as many as you want, use static {} syntax, called in order they are declared in the class
+            - All static initializers called before constructor
      */
     public static int multiplier = 7;
 
