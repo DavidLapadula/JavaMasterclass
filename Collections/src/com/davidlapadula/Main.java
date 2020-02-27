84275a3141396cfe88d20d677ecbf592de2f0050
DavidLapadula <lapadula.david@gmail.com>
2020-02-26 22:01:09 -0500

    [Collections] made basic seat reserving with list Collections

---
+++ Collections/src/com/davidlapadula/Main.java
@@ -0,0 +1,14 @@
+package com.davidlapadula;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    /*
+	        - Collections framework
+	            - Static methods work on collections
+	            - Interfaces - enforce methods on all collections and allow them to be extended
+	            - Allow interoperability between other collections
+	            - Algorithms that work on objects that implement Collections interface
+	     */
+    }
+}
