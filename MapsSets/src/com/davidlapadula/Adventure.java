e03bf8b2cf28b9caa8bcb6936922ef3ec97cea47
DavidLapadula <lapadula.david@gmail.com>
2020-03-09 22:17:30 -0400

    [Collections] Started simple location based adventure  game

--- MapsSets/src/com/davidlapadula/Adventure.java
+++ MapsSets/src/com/davidlapadula/Adventure.java
@@ -1,7 +1,30 @@
 package com.davidlapadula;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Adventure {
+    private static Map<Integer, Location> locations = new HashMap<>();
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        locations.put(1, new Location(1, "Location 1"));
+        locations.put(2, new Location(2, "Location 2"));
+        locations.put(3, new Location(3, "Location 3"));
+
+        int loc = 1;
+
+        while(true){
+            System.out.println(locations.get(loc).getDescription());
+            if (loc == 0) break;
+
+            loc = scanner.nextInt();
+            if (!locations.containsKey(loc)) {
+                System.out.println("Wrong direction");
+            }
+        }
 
     }
 }
---
+++ MapsSets/src/com/davidlapadula/Location.java
@@ -0,0 +1,33 @@
+package com.davidlapadula;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Location {
+    private final int locationID;
+    private final String description;
+    private final Map<String, Integer> exits;
+
+    public Location(int locationID, String description) {
+        this.locationID = locationID;
+        this.description = description;
+        this.exits = new HashMap<>();
+    }
+
+    public int getLocationID() {
+        return locationID;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Map<String, Integer> getExits() {
+        // Initialize new object and put existing object in the constructor. Prevents anything outside class from changing exits
+        return new HashMap<String, Integer>(exits);
+    }
+
+    public void addExit(String direction, int location){
+        exits.put(direction, location);
+    }
+}
