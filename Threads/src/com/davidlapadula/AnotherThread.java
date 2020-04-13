6a1d17626a819b65a60094cedae414b9d9d6751b
DavidLapadula <lapadula.david@gmail.com>
2020-04-13 12:14:06 -0400

    [concurrency] started working with threads

---
+++ Threads/src/com/davidlapadula/AnotherThread.java
@@ -0,0 +1,19 @@
+package com.davidlapadula;
+
+import static com.davidlapadula.ThreadColor.*;
+
+public class AnotherThread extends Thread {
+
+    @Override
+    public void run() {
+        System.out.println(ANSI_BLUE + "Hello from " + currentThread().getName());
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            System.out.println(ANSI_BLUE + "Another thread woke me up");
+            return;
+        }
+        System.out.println(ANSI_BLUE + "3 seconds passed an I am awake");
+    }
+}
