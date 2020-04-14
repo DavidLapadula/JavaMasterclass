412257d4146d565084290d5ea28f39c3e4216396
DavidLapadula <lapadula.david@gmail.com>
2020-04-14 15:47:11 -0400

    [concurrency] added producer/consumer solution and code using reentrant locks

---
+++ Messages/src/com/davidlapadula/Main.java
@@ -0,0 +1,63 @@
+package com.davidlapadula;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Message message = new Message();
+        (new Thread(new Writer(message))).start();
+        (new Thread(new Reader(message))).start();
+    }
+}
+
+class Writer implements Runnable {
+    private Message message;
+
+    public Writer(Message message) {
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        String messages[] = {
+                "Sitting on a wall",
+                "Had a great fall",
+                "All the people",
+                "Could not put back together"
+        };
+
+        Random random = new Random();
+
+        for (int i=0; i < messages.length; i++) {
+            message.write(messages[i]);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        message.write("Finished");
+    }
+}
+
+class Reader implements Runnable {
+    private Message message;
+
+    public Reader(Message message) {
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+        Random random = new Random();
+
+        for (String latestMessage = message.read(); !latestMessage.equals("Finished"); latestMessage = message.read()) {
+            System.out.println(latestMessage);
+            try {
+                Thread.sleep(random.nextInt(2000));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
