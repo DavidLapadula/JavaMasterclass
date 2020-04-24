4b799ca1bea3eefdbaf61fe77c179f4fc7dce277
DavidLapadula <lapadula.david@gmail.com>
2020-04-24 12:42:24 -0400

    [sqlite] implemented classes for retrieving data and creating views

--- Databases/TestDB/src/com/davidlapadula/Music.java
+++ Databases/TestDB/src/com/davidlapadula/Music.java
@@ -2,6 +2,8 @@ package com.davidlapadula;
 
 import com.davidlapadula.model.*;
 
+import java.util.List;
+
 public class Music {
 
     public static void main(String[] args) {
@@ -11,8 +13,28 @@ public class Music {
             System.out.println("Cant open");
             return;
         }
+        List<Artist> artists = dataSource.queryArtists(DataSource.ORDER_BY_ASC);
+        if (artists == null) {
+            System.out.println("Mo artists");
+            return;
+        }
+        for (Artist artist: artists) {
+            System.out.println(artist.getId() + "-" + artist.getName() + "-"  +artist.getName());
+        }
 
+        List <String> albumsForArtist = dataSource.queryAlbumsForArtist("Iron Maiden", DataSource.ORDER_BY_ASC);
+        for (String album: albumsForArtist) {
+            System.out.println(album);
+        }
 
+        List<SongArtist> songArtists = dataSource.queryArtistForSong("Heartless", DataSource.ORDER_BY_ASC);
+        if (songArtists != null) {
+            for (SongArtist artist: songArtists) {
+                System.out.println(artist.getArtistName() + "-" + artist.getAlbumName() + "-"  + artist.getTrack());
+            }
+        } else {
+            return;
+        }
 
         dataSource.close();
     }
