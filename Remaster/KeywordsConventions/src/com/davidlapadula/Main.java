bc06a96fb6459b0ac559a9065402d0fea6ff7dc9
DavidLapadula <lapadula.david@gmail.com>
2020-04-30 10:56:03 -0400

    Course Complete

---
+++ Remaster/KeywordsConventions/src/com/davidlapadula/Main.java
@@ -0,0 +1,14 @@
+package com.davidlapadula;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    double milesDriver = 1000d;
+	    int dailyCustomers = 50;
+	    char lastKey = 'd';
+
+	    int lives = 3;
+
+		System.out.println("Prefix for lives " + --lives);
+    }
+}
